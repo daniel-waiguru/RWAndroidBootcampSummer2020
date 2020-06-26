e757251a2cf8dd382b636a6d9197dccd9fe68b88
Daniel Waiguru <danielirungu91@gmail.com>
2020-06-26 14:17:45 +0300

    Added user validation

--- Week04/MoviesApp/app/src/main/java/tech/danielwaiguru/moviesapp/LoginFragment.kt
+++ Week04/MoviesApp/app/src/main/java/tech/danielwaiguru/moviesapp/LoginFragment.kt
@@ -1,17 +1,17 @@
 package tech.danielwaiguru.moviesapp
 
-import android.app.Activity
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import kotlinx.android.synthetic.main.fragment_login.*
 
 
 class LoginFragment : Fragment() {
-
+    private var isLoggedin: Boolean = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         (activity as AppCompatActivity).supportActionBar?.hide()
@@ -23,6 +23,17 @@ class LoginFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        btn_login.setOnClickListener {
+
+        }
+        txt_register.setOnClickListener {
+            val  action = LoginFragmentDirections.actionLoginFragmentToRegisterFragment2()
+            it.findNavController().navigate(action)
+        }
     }
 
     companion object {
--- Week04/MoviesApp/app/src/main/java/tech/danielwaiguru/moviesapp/RegisterFragment.kt
+++ Week04/MoviesApp/app/src/main/java/tech/danielwaiguru/moviesapp/RegisterFragment.kt
@@ -1,11 +1,14 @@
 package tech.danielwaiguru.moviesapp
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_register.*
 
 
 class RegisterFragment : Fragment() {
@@ -23,10 +26,51 @@ class RegisterFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_register, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        text_login.setOnClickListener {
+            val action = RegisterFragmentDirections.actionRegisterFragmentToLoginFragment2()
+            it.findNavController().navigate(action)
+        }
+        btn_register.setOnClickListener {
+            userDetailsValidation()
+        }
+    }
     companion object {
 
         @JvmStatic
         fun newInstance() = RegisterFragment()
 
+    }
+    private fun userDetailsValidation(){
+        if (usernameValidation() && passwordValidation()){
+            val action = LoginFragmentDirections.actionLoginFragmentToMovieFragment2()
+            findNavController().navigate(action)
+        }
+    }
+    private fun usernameValidation(): Boolean {
+
+        var isUsernameValid = true
+        etUsername.error = when {
+            etUsername.text.toString().isEmpty() -> {
+                isUsernameValid = false
+                "Username is required"
+            }
+            etUsername.text.toString().length < 5 -> {
+                isUsernameValid = false
+                "Username should be at least 5 chacters long"
+            }
+            else -> null
+        }
+        return isUsernameValid
+    }
+    private  fun passwordValidation(): Boolean {
+        var isPasswordValid = true
+        etPassword.error = if (etPassword.text.toString().length < 5){
+            isPasswordValid = false
+            "Password should at least 5 characters"
+        }
+        else null
+        return isPasswordValid
     }
 }
