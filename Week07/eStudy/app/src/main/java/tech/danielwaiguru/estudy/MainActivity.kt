4f1a23428e88ecc56a5f787e7686125f30ef7dee
Daniel Waiguru <danielirungu91@gmail.com>
2020-07-10 15:28:10 +0300

    Added Recyclerview Adapter

--- Week07/eStudy/app/src/main/java/tech/danielwaiguru/estudy/MainActivity.kt
+++ Week07/eStudy/app/src/main/java/tech/danielwaiguru/estudy/MainActivity.kt
@@ -1,11 +1,29 @@
 package tech.danielwaiguru.estudy
 
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
+import tech.danielwaiguru.estudy.adapters.BookAdapter
+import tech.danielwaiguru.estudy.viewmodels.BookViewModel
 
 class MainActivity : AppCompatActivity() {
+    private val bookAdapter by lazy {
+        BookAdapter()
+    }
+    private val bookViewModel by lazy {
+        ViewModelProvider(this).get(BookViewModel::class.java)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setUpRecyclerView()
+        bookViewModel.al
+    }
+    private fun setUpRecyclerView(){
+        books_rv.layoutManager = LinearLayoutManager(this)
+        books_rv.adapter = bookAdapter
     }
 }
--- Week07/eStudy/app/src/main/java/tech/danielwaiguru/estudy/adapters/BookAdapter.kt
+++ Week07/eStudy/app/src/main/java/tech/danielwaiguru/estudy/adapters/BookAdapter.kt
@@ -3,19 +3,26 @@ package tech.danielwaiguru.estudy.adapters
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import tech.danielwaiguru.estudy.R
 import tech.danielwaiguru.estudy.models.Book
 
-class BookAdapter: RecyclerView.Adapter<BookAdapter.BookViewHolder>() {
+class BookAdapter: ListAdapter<Book, BookAdapter.BookViewHolder>(BooksDiffCallback()) {
     private val booksList = mutableListOf<Book>()
-    inner class BookViewHolder(private val itemView: View): RecyclerView.ViewHolder(itemView) {
-
+    inner class BookViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+        val book_cover = itemView.findViewById<ImageView>(R.id.book_cover)
+        val title = itemView.findViewById<TextView>(R.id.book_title)
+        val subtitle = itemView.findViewById<TextView>(R.id.book_subtitle)
+        val publisher = itemView.findViewById<TextView>(R.id.book_publisher)
+        val date = itemView.findViewById<TextView>(R.id.date_published)
     }
     class BooksDiffCallback: DiffUtil.ItemCallback<Book>() {
         override fun areItemsTheSame(oldItem: Book, newItem: Book): Boolean {
-            TODO("Not yet implemented")
+            return oldItem.id == newItem.id
         }
 
         override fun areContentsTheSame(oldItem: Book, newItem: Book): Boolean {
@@ -35,12 +42,15 @@ class BookAdapter: RecyclerView.Adapter<BookAdapter.BookViewHolder>() {
         ))
     }
 
-    override fun getItemCount(): Int = booksList.size
 
     override fun onBindViewHolder(
         holder: BookViewHolder,
         position: Int
     ) {
-        TODO("Not yet implemented")
+        val book = booksList[position]
+        //holder.book_cover.setImageResource()
+        holder.title.text = book.volumeInfo.title
+        holder.publisher.text = book.publisher
+        holder.date.text = book.publishedDate
     }
 }
