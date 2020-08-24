b7d6233eb80fd93959a0125ceb7657476bc8fca2
Daniel Waiguru <danielirungu91@gmail.com>
2020-08-24 21:49:52 +0300

    fix: resolve failing ViewModel tests

--- Week04/MoviesApp/app/src/test/java/tech/danielwaiguru/moviesapp/viewmodels/MovieViewModelTest.kt
+++ Week04/MoviesApp/app/src/test/java/tech/danielwaiguru/moviesapp/viewmodels/MovieViewModelTest.kt
@@ -1,49 +1,40 @@
 package tech.danielwaiguru.moviesapp.viewmodels
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
-import androidx.lifecycle.Observer
+import com.github.testcoroutinesrule.TestCoroutineRule
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.verify
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.test.runBlockingTest
-import org.junit.Assert.assertNotNull
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
-import org.junit.rules.TestRule
-import org.junit.runner.RunWith
-import org.mockito.Mockito.mock
 import org.mockito.MockitoAnnotations
-import org.mockito.junit.MockitoJUnitRunner
-import tech.danielwaiguru.moviesapp.database.Movie
 import tech.danielwaiguru.moviesapp.repositories.MovieRepository
-import tech.danielwaiguru.moviesapp.utils.CoroutineTestRule
 
 
 @ExperimentalCoroutinesApi
-@RunWith(MockitoJUnitRunner::class)
 class MovieViewModelTest {
     @get:Rule
-    var rule: TestRule = InstantTaskExecutorRule()
-    lateinit var movieViewModel: MovieViewModel
-    @ExperimentalCoroutinesApi
+    val testCoroutineRule = TestCoroutineRule()
     @get:Rule
-    val testCoroutineRule = CoroutineTestRule()
-    lateinit var movieRepository: MovieRepository
-
-    lateinit var observer: Observer<List<Movie>>
-    private val movie = Movie(id = 1, poster_path = "", original_language = "en",
-        title = "Test Movie", vote_average = 2.4,
-        release_date = "2020-07-23", overview = "Test Movie overview")
-    private val result = listOf(movie)
+    val rule = InstantTaskExecutorRule()
+    lateinit var movieViewModel: MovieViewModel
+    private val movieRepository: MovieRepository = mock()
     @Before
     fun setup(){
         MockitoAnnotations.initMocks(this)
-        movieRepository = mock(MovieRepository::class.java)
         movieViewModel = MovieViewModel(movieRepository)
     }
-
     @Test
-    fun fetchMovies() = testCoroutineRule.testDispatcher.runBlockingTest {
-        val movies = movieViewModel.fetchMovies()
-        assertNotNull(movies)
+    fun `test fetchMovies function that gets movies from api`() {
+        testCoroutineRule.runBlockingTest {
+            movieViewModel.fetchMovies()
+            verify(movieRepository).fetchMovies(1)
+        }
+    }
+    @Test
+    fun `test getMovies function that gets movies from db`(){
+        movieViewModel.getAllMovies()
+        verify(movieRepository).movies
     }
 }
